a74cc598a9bec37135426cdeb6d15f12bbbf3bfe
Shakhzod Bobolov <shohbobolov98@gmail.com>
2025-01-12 17:07:22 +0500

    feat: initial project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { HomePage } from "./pages/home/home";
+
+function App() {
+  return (
+    <main className="bg-[#00302E] w-full h-screen">
+      <HomePage />
+      {/* <Navbar/> */}
+      {/* <Hero/> */}
+      {/* <About/> */}
+      {/* <Contact/> */}
+      {/* <Footer/> */}
+    </main>
+  );
+}
+
+export default App;
