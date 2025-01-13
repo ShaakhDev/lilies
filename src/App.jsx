ea32b29393b70074cbc15138bcddd02e899217b6
xayrullayevich2008 <izzatullayev008@gmail.com>
2025-01-13 19:33:25 +0500

    foods section

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-
 import "./App.css";
 import { Hero } from "./pages/hero/hero";
-
-
 
 import { Navbar } from "./pages/home/navbar";
 
 import { Footer } from "./pages";
 import Contact from "./pages/home/components/contact";
-
-
+import { Foods } from "./pages";
 // import { HomePage } from "./pages/home/home";
 
 function App() {
   return (
     <main className="bg-[#00302E] w-full h-screen">
       {/* <HomePage /> */}
-    <Navbar />
+      <Navbar />
       {/* <Navbar/> */}
-      <Hero/>
-      {/* <Contact/> */
+      <Hero />
+      <Foods />
+      {/* <Contact/> */}
       {/* <About/> */}
 
-  
       {/* <Hero/> */}
       {/* <About/> */}
       {/* <Contact/> */}
-
 
       {/* <Footer/> */}
       <Contact />
